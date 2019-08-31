918bfa047cfddfa4a6a080572221b31c5d99fd3b
ayoayan <ayodejiayankola@gmail.com>
2019-08-31 22:49:25 +0100

    installed and setup JWT auth

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+use Tymon\JWTAuth\Facades\JWTFactory;
+
+use Tymon\JWTAuth\Exceptions\JWTExceptions;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+use Tymon\JWTAuth\PayloadFactory;
+
+use Tymon\JWTAuth\JWTManager as JWT;
+
+class UserController extends Controller
+{
+    //
+}
